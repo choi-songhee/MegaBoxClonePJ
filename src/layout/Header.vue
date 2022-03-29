745316c3ab521c4fdf862217b28bbc91c795ebe0
songhee <songhee@dfluid.co.kr>
2022-03-29 18:18:20 +0900

    scss 세팅

--- src/layout/Header.vue
+++ src/layout/Header.vue
@@ -1,5 +1,9 @@
 <template>
-  <h1>header</h1>
+  <header class="header">
+    <h1 class="header__ci">
+      <a href="/" title="event main으로 가기" class="header__home-link">MEGABOX : Life Theater</a>
+    </h1>
+  </header>
 </template>
 
 <script>
@@ -8,6 +12,18 @@ export default {
 }
 </script>
 
-<style scoped>
+<style lang="scss" scoped>
+  .header {
+    position: relative;
+    z-index: 100;
+    width: 100%;
+    min-width: 1100px;
+    height: 91px;
+    background: #fff;
+    border-bottom: 1px solid #351f67;
 
+    &__home-link {
+      font-size: 14px;
+    }
+  }
 </style>
