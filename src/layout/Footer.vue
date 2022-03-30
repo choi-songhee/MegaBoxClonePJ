91a93215ec3cd480d89a174e2b80befce4e0aeda
kyongah <kyongah@dfluid.kr>
2022-03-30 13:29:12 +0900

    footer 작업중

--- src/layout/Footer.vue
+++ src/layout/Footer.vue
@@ -2,7 +2,7 @@
   <footer class="footer">
     <!--  footer-top  -->
     <div class="footer__top">
-      <div class="footer__inner-wrap">
+      <div class="finner-wrap">
         <ul class="fnb">
           <li class="fnb__li"><a href="#" title="회사소개 페이지로 이동" class="fnb__link">회사소개</a></li>
           <li class="fnb__li"><a href="#" title="인재채용 페이지로 이동" class="fnb__link">인재채용</a></li>
@@ -14,7 +14,7 @@
           <li class="fnb__li"><a href="#" target="_blank" title="윤리경영 페이지로 이동" class="fnb__link">윤리경영</a></li>
         </ul>
         <a href="#" class="search-theater" title="극장찾기">
-          <i></i>
+          <i class="icon-search icon-set"></i>
           극장찾기
         </a>
       </div>
@@ -23,12 +23,12 @@
 
     <!--  footer-bottom  -->
     <div class="footer__bottom">
-      <div class="footer__inner-wrap">
+      <div class="inner-wrap">
         <div class="ci">MEGABOX : LIFE Theater</div>
         <div class="footer__info">
-          <div>
-            <address>서울특별시 마포구 월드컵로 240, 지상2층(성산동, 월드컵주경기장)</address>
-            <span>ARS 1544-0070</span>
+          <div class="address">
+            <address class="address__info">서울특별시 마포구 월드컵로 240, 지상2층(성산동, 월드컵주경기장)</address>
+            <span class="address__tel">ARS 1544-0070</span>
           </div>
           <span>대표자명 홍정인</span>
           <span>· 개인정보보호책임자 공성진</span>
@@ -37,11 +37,21 @@
           <p class="copyright">COPYRIGHT © MegaboxJoongAng, Inc. All rights reserved</p>
         </div>
         <div class="footer__sns">
-          <a href="https://twitter.com/megaboxon" target="_blank" title="megabox 트위터 페이지로 이동"><i></i></a>
-          <a href="https://www.facebook.com/megaboxon" target="_blank" title="megabox 페이스북 페이지로 이동"><i>페이스북</i></a>
-          <a href="http://instagram.com/megaboxon" target="_blank" title="megabox 인스타그램 페이지로 이동"><i>인스타그램</i></a>
-          <a href="https://play.google.com/store/apps/details?id=com.megabox.mop" target="_blank" title="megabox 구글플레이 페이지로 이동"><i>구글플레이</i></a>
-          <a href="https://itunes.apple.com/kr/app/megabox/id894443858?l=ko&ls=1&mt=8" target="_blank" title="megabox 앱스토어 페이지로 이동"><i>앱스토어</i></a>
+          <a href="https://twitter.com/megaboxon" target="_blank" title="megabox 트위터 페이지로 이동" class="footer__sns-link">
+            <i class="icon-twitter icon-set">트위터</i>
+          </a>
+          <a href="https://www.facebook.com/megaboxon" target="_blank" title="megabox 페이스북 페이지로 이동" class="footer__sns-link">
+            <i class="icon-facebook icon-set">페이스북</i>
+          </a>
+          <a href="http://instagram.com/megaboxon" target="_blank" title="megabox 인스타그램 페이지로 이동" class="footer__sns-link">
+            <i class="icon-instagram icon-set">인스타그램</i>
+          </a>
+          <a href="https://play.google.com/store/apps/details?id=com.megabox.mop" target="_blank" title="megabox 구글플레이 페이지로 이동" class="footer__sns-link">
+            <i class="icon-googleplay icon-set">구글플레이</i>
+          </a>
+          <a href="https://itunes.apple.com/kr/app/megabox/id894443858?l=ko&ls=1&mt=8" target="_blank" title="megabox 앱스토어 페이지로 이동" class="footer__sns-link">
+            <i class="icon-appstore icon-set">앱스토어</i>
+          </a>
         </div>
       </div>
     </div>
@@ -56,7 +66,7 @@ export default {
 </script>
 
 <style lang="scss" scoped>
-.footer{
+.footer {
   position: relative;
   min-width: 1100px;
   width: 100%;
@@ -66,25 +76,16 @@ export default {
   background-color: #f8f8fa;
   box-sizing: border-box;
 
-  &__top{
+  &__top {
     overflow: hidden;
     width: 1100px;
     margin: 0 auto;
     padding: 30px 0;
   }
 
-  &__inner-wrap{
-    width: 1100px;
+  .inner-wrap {
     margin: 0 auto;
-
-    &:before{
-      content: '';
-      display: table;
-    }
-
-    &:after{
-      clear: both;
-    }
+    width: 1100px;
   }
 
   .fnb {
@@ -106,8 +107,7 @@ export default {
       display: block;
       color: #666;
       line-height: 30px;
-      font-size: .8667em;
-      text-decoration: none;
+      font-size: 13px;
     }
 
     &__li--privacy {
@@ -116,7 +116,8 @@ export default {
     }
   }
 
-  .search-theater{
+  .search-theater {
+    cursor: pointer;
     overflow: hidden;
     float: right;
     display: block;
@@ -125,12 +126,105 @@ export default {
     text-align: center;
     line-height: 28px;
     color: #666;
-    font-size: .8667em;
+    font-size: 13px;
     border: 1px solid #d8d9db;
     border-radius: 30px;
     text-decoration: none;
   }
+
+  .icon-search {
+    margin-right: 4px;
+    width: 15px;
+    height: 15px;
+    background-image: url('../assets/pc/icon_footer_search.png');
+  }
+
+  .icon-set{
+    overflow: hidden;
+    display: inline-block;
+    padding: 0;
+    font-size: 0;
+    line-height: 0;
+    vertical-align: middle;
+    background-position: 0 0;
+    background-repeat: no-repeat;
+  }
+
+  &__bottom {
+    overflow: hidden;
+    width: 1100px;
+    margin: 0 auto;
+    padding: 0;
+  }
+
+  .ci {
+    overflow: hidden;
+    display: block;
+    float: left;
+    width: 150px;
+    height: 50px;
+    margin: 0 20px 0 0;
+    padding: 0;
+    text-indent: -9999px;
+    background: url('../assets/pc/icon_logo_opacity.png');
+  }
+
+  &__info {
+    float: left;
+    font-size: 13px;
+    letter-spacing: 0;
+  }
+
+  .address{
+    &__info{
+      display: inline-block;
+      font-style: normal;
+    }
+  }
+
+  .copyright{
+    margin: 0;
+  }
+
+  &__sns{
+    float: right;
+  }
+
+  &__sns-link{
+    display: block;
+    float: left;
+    width: 32px;
+    height: 30px;
+    margin-left: 5px;
+    font-size: 0;
+    line-height: 0;
+  }
+
+  .icon-twitter{
+      width: 32px;
+      height: 30px;
+      background-image: url("../assets/pc/icon_twitter.png");
+    }
+  .icon-facebook{
+      width: 32px;
+      height: 30px;
+      background-image: url("../assets/pc/icon_facebook.png");
+    }
+  .icon-instagram{
+      width: 32px;
+      height: 30px;
+      background-image: url("../assets/pc/icon_instagram.png");
+    }
+  .icon-googleplay{
+      width: 32px;
+      height: 30px;
+      background-image: url("../assets/pc/icon_googleplay.png");
+    }
+  .icon-appstore{
+      width: 32px;
+      height: 30px;
+      background-image: url("../assets/pc/icon_appstore.png");
+    }
+
 }
-
-
 </style>
