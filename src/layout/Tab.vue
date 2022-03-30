11ab9e06e1217121e4bc639fe879f63eb9cc070b
dabin <dabin@dfluid.kr>
2022-03-30 10:10:35 +0900

    테스트 푸시

---
+++ src/layout/Tab.vue
@@ -0,0 +1,34 @@
+<template>
+  <div>
+    <b-tabs content-class="mt-3">
+      <b-tab title="전체" active>
+        <p>I'm the first tab</p>
+      </b-tab>
+      <b-tab title="메가Pick">
+        <p>I'm the second tab</p>
+      </b-tab>
+      <b-tab title="영화">
+        <p>I'm the second tab</p>
+      </b-tab>
+      <b-tab title="극장">
+        <p>I'm the second tab</p>
+      </b-tab>
+      <b-tab title="제휴/할인">
+        <p>I'm the second tab</p>
+      </b-tab>
+      <b-tab title="시사회/무대인사">
+        <p>I'm the second tab</p>
+      </b-tab>
+    </b-tabs>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Tab'
+}
+</script>
+
+<style scoped>
+
+</style>
