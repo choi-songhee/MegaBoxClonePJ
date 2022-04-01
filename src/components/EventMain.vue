35c30722d2044115a82a6cc2ae600e124092e77c
kyongah <kyongah@dfluid.kr>
2022-04-01 17:24:58 +0900

    swiper 작업중

--- src/components/EventMain.vue
+++ src/components/EventMain.vue
@@ -1,143 +1,143 @@
+
 <template>
   <div class="container">
     <Location :lists="depthList"/>
-      <div class="contents">
-        <div class="inner">
-          <h1 class="contents__title">진행중인 이벤트</h1>
-          <div class="tabs">
-            <Tab
-              v-for="item in list"
-              v-bind="item" :key="item.id"
-              v-model="currentId"
-            />
+    <div class="contents">
+      <div class="inner">
+        <h1 class="contents__title">진행중인 이벤트</h1>
+        <div class="tabs">
+          <Tab
+            v-for="item in list"
+            v-bind="item" :key="item.id"
+            v-model="currentId"
+          />
+        </div>
+      </div>
+      <div class="tab-content">
+        <div v-show="currentId == 0">
+          <div class="contents__event-slider">
+            <div>
+              <h3 class="contents__sub-title">추천 이벤트</h3>
+              <div
+                class="swiper-pagination"
+                slot="pagination"
+              >
+              </div>
+            </div>
+            <swiper
+              class="event-swiper"
+              :options="swiperOption"
+            >
+              <swiper-slide>Slide 1</swiper-slide>
+              <swiper-slide>Slide 2</swiper-slide>
+              <swiper-slide>Slide 3</swiper-slide>
+              <div class="swiper-button-prev" slot="button-prev"></div>
+              <div class="swiper-button-next" slot="button-next"></div>
+            </swiper>
+          </div>
+          <div class="inner">
+            <h3 class="contents__sub-title">메가Pick</h3>
+            <ul class="event">
+              <li v-for="item in megaPickLists" :key="item.id" class="event__list">
+                <ListItem :item="item"/>
+              </li>
+            </ul>
+            <h3 class="contents__sub-title">영화</h3>
+            <ul class="event">
+              <li v-for="item in megaPickLists" :key="item.id" class="event__list">
+                <ListItem :item="item"/>
+              </li>
+            </ul>
+            <h3 class="contents__sub-title">극장</h3>
+            <ul class="event">
+              <li v-for="item in megaPickLists" :key="item.id" class="event__list">
+                <ListItem :item="item"/>
+              </li>
+            </ul>
+            <h3 class="contents__sub-title">제휴/할인</h3>
+            <ul class="event">
+              <li v-for="item in megaPickLists" :key="item.id" class="event__list">
+                <ListItem :item="item"/>
+              </li>
+            </ul>
+            <h3 class="contents__sub-title">시사회/무대인사</h3>
+            <ul class="event">
+              <li v-for="item in megaPickLists" :key="item.id" class="event__list">
+                <ListItem :item="item"/>
+              </li>
+            </ul>
           </div>
         </div>
-        <div class="tab-content">
-          <div v-show="currentId == 0">
-            <div class="inner">
-              <h3 class="contents__sub-title">메가Pick</h3>
-              <ul class="event">
-                <li v-for="item in megaPickLists" :key="item.id" class="event__list">
-                  <ListItem :item="item"/>
-                </li>
-              </ul>
-
-              <h3 class="contents__sub-title">영화</h3>
-              <ul class="event">
-                <li v-for="item in megaPickLists" :key="item.id" class="event__list">
-                  <ListItem :item="item"/>
-                </li>
-              </ul>
-
-              <h3 class="contents__sub-title">극장</h3>
-              <ul class="event">
-                <li v-for="item in megaPickLists" :key="item.id" class="event__list">
-                  <ListItem :item="item"/>
-                </li>
-              </ul>
-
-              <h3 class="contents__sub-title">제휴/할인</h3>
-              <ul class="event">
-                <li v-for="item in megaPickLists" :key="item.id" class="event__list">
-                  <ListItem :item="item"/>
-                </li>
-              </ul>
-
-              <h3 class="contents__sub-title">시사회/무대인사</h3>
-              <ul class="event">
-                <li v-for="item in megaPickLists" :key="item.id" class="event__list">
-                  <ListItem :item="item"/>
-                </li>
-              </ul>
-            </div>
-          </div>
-          <div v-show="currentId == 1">
-            <div class="inner">
-              <SearchArea></SearchArea>
-              <ul class="event">
-                <li v-for="item in megaPickLists" :key="item.id" class="event__list">
-                  <ListItem :item="item"/>
-                </li>
-              </ul>
-            </div>
-          </div>
-          <div v-show="currentId == 2">
-            <div class="inner">
-              <div class="sub-tab">
-                <ul class="sub-tab__lists">
-                  <li v-for="(tab, index) in subTabs" :key="index" class="sub-tab__list" @click="onClickTab(index)">
-                    <router-link :to="tab.path" :class="['sub-tab__link', {'is-active': index === selectedTab}]">{{ tab.name }}</router-link>
-                  </li>
-                </ul>
-              </div>
-              <SearchArea></SearchArea>
-              <ul class="event">
-                <li v-for="item in megaPickLists" :key="item.id" class="event__list">
-                  <ListItem :item="item"/>
-                </li>
-              </ul>
-            </div>
-          </div>
-          <div v-show="currentId == 3">
-            <div class="inner">
-              <SearchArea></SearchArea>
-              <ul class="event">
-                <li v-for="item in megaPickLists" :key="item.id" class="event__list">
-                  <ListItem :item="item"/>
-                </li>
-              </ul>
-            </div>
-          </div>
-          <div v-show="currentId == 4">
-            <div class="inner">
-              <SearchArea></SearchArea>
-              <ul class="event">
-                <li v-for="item in megaPickLists" :key="item.id" class="event__list">
-                  <ListItem :item="item"/>
-                </li>
-              </ul>
-            </div>
-          </div>
-          <div v-show="currentId == 5">
-            <div class="inner">
+        <div v-show="currentId == 1">
+          <div class="inner">
             <SearchArea></SearchArea>
-              <ul class="event">
-                <li v-for="item in megaPickLists" :key="item.id" class="event__list">
-                  <ListItem :item="item"/>
+            <ul class="event">
+              <li v-for="item in megaPickLists" :key="item.id" class="event__list">
+                <ListItem :item="item"/>
+              </li>
+            </ul>
+          </div>
+        </div>
+        <div v-show="currentId == 2">
+          <div class="inner">
+            <div class="sub-tab">
+              <ul class="sub-tab__lists">
+                <li v-for="(tab, index) in subTabs" :key="index" class="sub-tab__list" @click="onClickTab(index)">
+                  <router-link :to="tab.path" :class="['sub-tab__link', {'is-active': index === selectedTab}]">{{ tab.name }}</router-link>
                 </li>
               </ul>
             </div>
+            <SearchArea></SearchArea>
+            <ul class="event">
+              <li v-for="item in megaPickLists" :key="item.id" class="event__list">
+                <ListItem :item="item"/>
+              </li>
+            </ul>
           </div>
-          <swiper
-            class="swiper"
-            :options="swiperOption"
-          >
-            <swiper-slide>Slide 1</swiper-slide>
-            <swiper-slide>Slide 2</swiper-slide>
-            <swiper-slide>Slide 3</swiper-slide>
-            <div
-              class="swiper-pagination"
-              slot="pagination"
-            >
-            </div>
-            <div class="swiper-button-prev" slot="button-prev"></div>
-            <div class="swiper-button-next" slot="button-next"></div>
-          </swiper>
+        </div>
+        <div v-show="currentId == 3">
+          <div class="inner">
+            <SearchArea></SearchArea>
+            <ul class="event">
+              <li v-for="item in megaPickLists" :key="item.id" class="event__list">
+                <ListItem :item="item"/>
+              </li>
+            </ul>
+          </div>
+        </div>
+        <div v-show="currentId == 4">
+          <div class="inner">
+            <SearchArea></SearchArea>
+            <ul class="event">
+              <li v-for="item in megaPickLists" :key="item.id" class="event__list">
+                <ListItem :item="item"/>
+              </li>
+            </ul>
+          </div>
+        </div>
+        <div v-show="currentId == 5">
+          <div class="inner">
+            <SearchArea></SearchArea>
+            <ul class="event">
+              <li v-for="item in megaPickLists" :key="item.id" class="event__list">
+                <ListItem :item="item"/>
+              </li>
+            </ul>
+          </div>
         </div>
       </div>
     </div>
+  </div>
 </template>
 
 <script>
-import ListItem from './ListItem'
 import Tab from './Tab'
 import SearchArea from './SearchArea'
 import MoreButton from './MoreButton'
 import Location from './Location'
-
+import ListItem from './ListItem'
 import { Swiper, SwiperSlide } from 'vue-awesome-swiper'
 import 'swiper/css/swiper.css'
-
 export default {
   name: 'EventMain',
   components: {
@@ -205,13 +205,27 @@ export default {
           alt: '2022 교향악축제 이미지',
           title: '[클래식소사이어티] 2022 교향악축제 초대 이벤트',
           date: '2022.03.30 ~ 2022.04.13'
+        },
+        {
+          src: '../../static/dummy/images/img_dummy03.jpeg',
+          alt: '봄시즌음료 이미지',
+          title: '[봄시즌음료]딸기라떼, 옥수수라떼 론칭',
+          date: '2022.03.30 ~ 2022.04.13'
+        },
+        {
+          src: '../../static/dummy/images/img_dummy04.jpeg',
+          alt: '2022년 VIP 회원 혜택 이미지',
+          title: '✌ 2022년 VIP 회원 혜택 안내 ✌',
+          date: '2022.03.30 ~ 2022.04.13'
+        }
+      ],
       swiperOption: {
-        slidesPerView: 1,
-        spaceBetween: 30,
+        slidesPerView: 2,
+        spaceBetween: 40,
         loop: true,
         pagination: {
           el: '.swiper-pagination',
-          clickable: true
+          type: 'fraction'
         },
         navigation: {
           nextEl: '.swiper-button-next',
@@ -240,13 +254,11 @@ export default {
 .sub-tab {
   position: relative;
   padding: 30px 0;
-
   &__lists::after {
     content: '';
     display: block;
     clear: both;
   }
-
   &__list {
     float: left;
     position: relative;
@@ -254,7 +266,6 @@ export default {
     line-height: 16px;
     padding-right: 16px;
     margin-right: 15px;
-
     &::before {
       content: '';
       position: absolute;
@@ -265,17 +276,14 @@ export default {
       background-color: #d8d9db;
     }
   }
-
   &__link {
     display: inline-block;
     height: 23px;
     font-size: 16px;
-
     &.is-active {
       border-bottom: 2px solid #503396;
       color: #503396;
     }
-
     &:not(.is-active) {
       &:active,
       &:focus,
@@ -290,10 +298,23 @@ export default {
   margin: 0 -20px;
   display: flex;
   justify-content: flex-start;
-
   &__list {
     flex: 0 0 245px;
     margin: 0 20px;
   }
+}
+.contents__event-slider {
+  position: relative;
+  height: 402px;
+  margin: 40px 0 0 0;
+  background-color: #f8f8fa;
+}
+.event-swiper{
+  overflow: hidden;
+  position: absolute;
+  left: 50%;
+  width: 1110px;
+  margin: 0 0 0 -550px;
+  padding: 0 10px 0 0;
 }
 </style>
